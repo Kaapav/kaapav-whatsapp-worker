5e23748944c5d687a8bd971355e3ce9074317dc8
Kaapav <kaapavin@gmail.com>
2025-07-11 19:50:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -122,9 +122,9 @@ async function handleGPTandCRM(data) {
 
     // 2️⃣ Tiledesk Push (Auth for Tiledesk)
     const projectId = process.env.TILEDESK_PROJECT_ID || "686922633c8e640013d7e9ec";
-    const requestId = `whatsapp-${wa_id}`;
+    const requestId =  `support-group-${wa_id}`;
     const authURL = "https://api.tiledesk.com/v3/auth/signinAnonymously";
-    const pushURL = `https://api.tiledesk.com/v3/${projectId}/requests/${requestId}/messages`;
+    const pushURL = `https://api.tiledesk.com/v3/<project_id>/requests/support-group-919148330066/messages`;
     
     const { data: auth } = await axios.post(authURL, {
       id_project: projectId,
