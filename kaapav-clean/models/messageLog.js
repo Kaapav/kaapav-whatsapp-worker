07b2738f4b08ab019a125a25e8b7860a5d574ed0
Kaapav <kaapavin@gmail.com>
2025-07-31 17:00:27 +0530

    Delete kaapav-clean directory

--- kaapav-clean/models/messageLog.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const messageLogSchema = new mongoose.Schema({
-  from: String,
-  text: String,
-  timestamp: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('MessageLog', messageLogSchema);
