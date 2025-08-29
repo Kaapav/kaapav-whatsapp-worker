8333ecc425e7781d3bdbd90772ec21a40d6dfdec
Kaapav <kaapavin@gmail.com>
2025-08-30 01:54:26 +0530

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,14 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: "kaapav-bot",
+      name: "backend",
       script: "index.js",
-      cwd: "/home/ubuntu/kaapav-whatsapp-worker/backend",
-      watch: true,
+      cwd: "./backend",   // runs from backend folder
+      instances: 1,
+      autorestart: true,
+      watch: false,
       env: {
         NODE_ENV: "production",
-        PORT: 5555
+        PORT: 5555,
+
+        // WhatsApp Cloud API
+        WHATSAPP_ACCESS_TOKEN: "EAAI6dZCuTmYYBPFs5zVM7jPrDTAsHsyjHsl2d6ZAc5do7BpnwwydjXL6L7tvuIFSauTZBxoCogmKvtJWKwDkofXjI0mZAbPkLQ7fwsBMgG3XioKA1MqePVYK6h8P6IZCFBow6qHpw6nZC7zPH3ITdnaWrxOj4ZBORhjPmj9Yxlz0Kej6UFHOS4sbVTItWllYrPcZAgZDZD",
+        WHATSAPP_PHONE_NUMBER_ID: "745230991999571",
+        GRAPH_API_VERSION: "v17.0",
+
+        // Optional integrations
+        MONGO_URI: "mongodb+srv://kaapavin:Kaapav%40123%21@cluster0.usauxbv.mongodb.net/tiledesk?retryWrites=true&w=majority&appName=Cluster0&connectTimeoutMS=30000",
+        SHEETS_ENABLED: "0"
       }
     }
   ]
-};
+}
