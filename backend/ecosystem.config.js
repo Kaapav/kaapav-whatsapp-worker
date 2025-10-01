c6c7d65257e70393e28a5e1a23b2b7dec3baa4f3
Kaapav <kaapavin@gmail.com>
2025-10-01 17:34:55 +0530

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,6 +11,7 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 5555,
 ADMIN_TOKEN=206f44c014062aed4982a3c25fe61a5b
+   BASE_URL: "https://www.crm.kaapav.com",
         // WhatsApp Cloud API
         WHATSAPP_ACCESS_TOKEN: "EAAI6dZCuTmYYBPFs5zVM7jPrDTAsHsyjHsl2d6ZAc5do7BpnwwydjXL6L7tvuIFSauTZBxoCogmKvtJWKwDkofXjI0mZAbPkLQ7fwsBMgG3XioKA1MqePVYK6h8P6IZCFBow6qHpw6nZC7zPH3ITdnaWrxOj4ZBORhjPmj9Yxlz0Kej6UFHOS4sbVTItWllYrPcZAgZDZD",
         WHATSAPP_PHONE_NUMBER_ID: "745230991999571",
