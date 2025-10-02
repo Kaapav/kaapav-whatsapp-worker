01439537594016a5cf3c508fc556455c252da950
Kaapav <kaapavin@gmail.com>
2025-10-02 22:54:26 +0530

    Update main.jsx

--- kaapav-chat-ui/src/main.jsx
+++ kaapav-chat-ui/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import AdminWhatsAppPanel from "./AdminWhatsAppPanel";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AdminWhatsAppPanel />
   </React.StrictMode>
 );
